b78ec7e38b272f24dede7e893853859ca07f4f19
PRIYADHARSHINI V <92779705+priascode@users.noreply.github.com>
2023-09-12 16:09:41 +0530

    practice

---
+++ python/Basics/AreaOfCircle.py
@@ -0,0 +1,4 @@
+from math import pi
+r = float(input("Enter radius:"))
+Area = pi*r*r
+print("Area of the circle is:",Area)
---
+++ python/Basics/fnamelname.py
@@ -0,0 +1,3 @@
+fname=input("Enter your firstname:")
+lname= input("Enter your lastname:")
+print("Hello "+fname+" "+lname)
